3c5ac4fc7e4c39fc506687ab7ff61745a8908051
natalijakatauske <natalija.katauske@gmail.com>
2022-09-26 19:26:28 +0300

    pabaigta uzduotis

--- scripts.js
+++ scripts.js
@@ -17,15 +17,35 @@ console.log(Object.keys(person))
 // const myJSON = JSON.stringify(person)
 // localStorage.setItem("test", myJSON)
 
-const myName = document.getElementById('name')
-const mySurname = document.getElementById('surname')
-const myAge = document.getElementById('age')
-
-document.querySelector('form').addEventListener('submit', myFunction)
-function myFunction(event) {
+document.querySelector('form').addEventListener('submit', submitFunction)
+function submitFunction(event) {
     event.preventDefault()
 
-    for (let key in person) {
-        console.log(key, person[key])
-    }
+    const myName = document.getElementById('name').value
+    const mySurname = document.getElementById('surname').value
+    const myAge = document.getElementById('age').value
+    const object = {myName, mySurname, myAge}
+    console.log(object)
+    console.log(typeof object)
+
+    myJSON = JSON.stringify(object)
+    console.log(myJSON)
+    localStorage.setItem('testJSON', myJSON)
+}
+
+document.querySelector('form').addEventListener('reset', resetFunction)
+function resetFunction(event) {
+    event.preventDefault()
+    localStorage.removeItem('testJSON')
+    console.log('resetttt')
+}
+
+document.getElementById('showAll').addEventListener('click', showAllFunction)
+function showAllFunction(event) {
+    event.preventDefault()
+    console.log('show all')
+    const info = localStorage.getItem('testJSON')
+    const newObject = JSON.parse(info)
+    console.log(newObject)
+    document.getElementById('output').innerText += newObject.myName + ' ' + newObject.mySurname + ' ' + newObject.myAge
 }
